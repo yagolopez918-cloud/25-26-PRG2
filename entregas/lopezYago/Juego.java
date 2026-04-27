fa2393fdae38a1167685d679230a0f55369334c5
yagolopez918-cloud <yagolopez918@gmail.com>
2026-04-27 17:54:58 +0200

    he olvidado de los babysteps a partir de ahora te los hago

--- entregas/lopezYago/Juego.java
+++ entregas/lopezYago/Juego.java
@@ -1,4 +1,4 @@
-package entregas.lopezYago;
+
 
 class Juego{
     public static void main(String[] args) {
--- entregas/lopezYago/Pista.java
+++ entregas/lopezYago/Pista.java
@@ -1,21 +1,54 @@
-package entregas.lopezYago;
-
 public class Pista {
+    private int[] posiciones;
+    private final int META = 40;
+
+    public Pista(int numeroCaballos) {
+        posiciones = new int[numeroCaballos];
+        for (int i = 0; i < numeroCaballos; i++) {
+            posiciones[i] = 0;
+        }
+    }
+
+    public void mover(int caballo, int avance) {
+        posiciones[caballo] = posiciones[caballo] + avance;
+        if (posiciones[caballo] > META) {
+            posiciones[caballo] = META;
+        }
+    }
 
     public void mostrarResultadoFinal() {
-       
+        System.out.println("Resultado final:");
+        for (int i = 0; i < posiciones.length; i++) {
+            if (posiciones[i] >= META) {
+                System.out.println("Ganó el caballo " + (i + 1));
+            }
+        }
     }
 
     public boolean hayGanador() {
         return false;
     }
+
     public boolean hayGanadores() {
         return false;
-    
+
     }
 
     public void mostrar() {
-        
+        for (int i = 0; i < META; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+        for (int i = 0; i < posiciones.length; i++) {
+            for (int j = 0; j < posiciones[i]; j++) {
+                System.out.print(" ");
+            }
+            System.out.println(";-;'");
+        }
+        for (int i = 0; i < META; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
-    
+
 }
